8b3132b6b97188b0820bd3fbed07ca3a1bb5b438
ssebastianoo <sebastianogirotto05@gmail.com>
2021-08-21 02:39:19 +0200

    guess the element

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
-if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-  document.getElementById("css").href = "css/mobile.css";
-};
+let mode = 'desktop';
+
+window.onload = checkDevice;
+window.onresize = checkDevice;
+
+function checkDevice() {
+  if (mode === 'desktop') {
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+      mode = 'mobile';
+      return document.getElementById("css").href = "css/mobile.css";
+    } else if (window.innerHeight >= window.innerWidth) {
+      mode = 'mobile';
+      return document.getElementById("css").href = "css/mobile.css";
+    };
+  } else {
+    if (window.innerHeight <= window.innerWidth) {
+      mode = 'desktop';
+      return document.getElementById("css").href = "css/style.css";
+    };
+  };
+}
 
 var line = true;
 var terminal = setInterval(function() {
