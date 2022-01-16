6510dd102a98a4860b98f45b12ea07d1354c1bd1
ssebastianoo <sebastianogirotto05@gmail.com>
2022-01-16 11:29:06 +0100

    new website

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react';
 import SebPic from './cdn/logo.png';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
@@ -33,30 +34,36 @@ function Social(props) {
  )
 }
 
-function App() {
-  return (
-    <div className="App">
-      <div className="Info">
-        <div className="SebPic">
-          <img src={SebPic} alt="seb"/>
+class App extends React.Component {
+  componentDidMount() {
+    document.title = 'seb';
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="Info">
+          <div className="SebPic">
+            <img src={SebPic} alt="seb"/>
+          </div>
+          <div className="InfoText">
+            <h2>im seb</h2>
+            <p><Box color="#f7e018" text="javascript"/> && <Box color="#3474a7" text="python"/> developer</p>
+          </div>
         </div>
-        <div className="InfoText">
-          <h2>im seb</h2>
-          <p><Box color="#f7e018" text="javascript"/> && <Box color="#3474a7" text="python"/> developer</p>
+        <div className="Socials">
+          <Social url="https://twitter.com/ssebastianoo" icon={faTwitter} />
+          <Social url="https://github.com/ssebastianoo" icon={faGithub} />
+        </div>
+        <h1 className="ProjectsTitle">projects</h1>
+        <div className="Projects">
+          <Project title="guess the element" description="guess the random element from the periodic table" url="https://seba.gq/GuessTheElement" />
+          <Project title="cookie fighter" description="catch the cookies on discord" url="https://cookie-is.the-be.st/"/>
+          <Project title="ram monitor" description="webapp to monitor your ram usage" url="https://github.com/ToastEnergy/RamMonitor"/>
         </div>
       </div>
-      <div className="Socials">
-        <Social url="https://twitter.com/ssebastianoo" icon={faTwitter} />
-        <Social url="https://github.com/ssebastianoo" icon={faGithub} />
-      </div>
-      <h1 style={{textAlign: 'center', margin: "50px"}}>projects</h1>
-      <div className="Projects">
-        <Project title="guess the element" description="guess the random element from the periodic table" url="https://seba.gq/GuessTheElement" />
-        <Project title="cookie fighter" description="catch the cookies on discord" url="https://cookie-is.the-be.st/"/>
-        <Project title="ram monitor" description="webapp to monitor your ram usage" url="https://github.com/ToastEnergy/RamMonitor"/>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
